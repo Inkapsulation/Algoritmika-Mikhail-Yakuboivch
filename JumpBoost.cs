141a6a90e9bfa333a04cf1cd996ca6b6e0b4f327
Inkapsulation <111992973+Inkapsulation@users.noreply.github.com>
2022-11-30 20:56:06 +0500

    Add files via upload

--- JumpBoost.cs
+++ JumpBoost.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class JumpBoost : MonoBehaviour
 {
     public float jumpBoost = 10.5f;
+    Vector3 newPosition;
 
     private void OnTriggerEnter(Collider other)
     {
         other.GetComponent<Jump>().jumpStrength *= jumpBoost;
+        
     }
 
     private void OnTriggerExit(Collider other)
     {
         other.GetComponent<Jump>().jumpStrength /= jumpBoost;
+        newPosition.x = other.GetComponent<Transform>().position.x * Time.deltaTime; //+= transform.position.y * Time.deltaTime;
+        other.GetComponent<Transform>().position = newPosition;
     }
 }
